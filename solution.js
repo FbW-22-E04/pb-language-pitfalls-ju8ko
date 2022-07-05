87b1cf3577ccbd7b730fe39ce4aa405067b02582
ju8ko <julia.x.kobzar@gmail.com>
2022-07-05 14:29:20 +0200

    The exercise is done

--- solution.js
+++ solution.js
@@ -1,1 +1,13 @@
+//1
+//The global NaN property is a value representing Not-A-Number.
+console.log("Ex.1.1:", isNaN("hello"));
+console.log("Ex.1.2:", isNaN(3));
+console.log("Ex.1.3:", typeof NaN);
 
+//2
+console.log((0.1 * 0.2).toFixed(2));
+
+//3
+console.log("Ex.3.1:", Infinity / 0);
+console.log("Ex.3.2:", Infinity / Infinity);
+console.log("Ex.3.3:", 1 / 0);
